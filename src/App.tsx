79f00b3c4029901f6f8eaa7cce65cad4d612e4f0
rawanelkhodary <relkhodary@smart.sa>
2024-10-08 14:02:23 +0300

    init structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import MainLogo from "/images/icon.png";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Layout from "./layout/Layout";
 function App() {
-  return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Saudi Events</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <div className="card"></div>
-      <p className="read-the-docs">Reads Docs</p>
-      <footer>
-        <img src={MainLogo} width="60px" height="60px" alt="MainLogo" />
-      </footer>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import EventList from "../components/event-list/EventList";
+
+const Layout = () => {
+  return (
+    <>
+      <EventList />
+    </>
+  );
+};
+
+export default Layout;
