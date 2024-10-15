4b3ee84ded8e80912bd3c2c2f0810a1cd4e15762
rawanelkhodary <relkhodary@smart.sa>
2024-10-15 10:31:27 +0300

    "Added react-router-dom dependency and implemented routing in App.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import Layout from "./layout/Layout";
 import "bootstrap/dist/js/bootstrap.bundle.min"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
-  return (<Layout />);
+  return ( <Router>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+  </Router>);
 }
 
 export default App;
